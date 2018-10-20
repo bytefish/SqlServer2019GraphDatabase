74f308b02ec45fea042603ef251ff5f66b99a852
Philipp Wagner <pwag86@googlemail.com>
2018-10-20 10:16:30 +0200

    Fix JsonProperty Names

--- CSharp/SqlServer2019Graph/Model/Edge.cs
+++ CSharp/SqlServer2019Graph/Model/Edge.cs
@@ -1,12 +1,23 @@
 ﻿// Copyright (c) Philipp Wagner. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
 namespace SqlServer2019Graph.Model
 {
     public class Edge
     {
+        [JsonProperty("edge_table_name")]
+        public string Name { get; set; }
+
+        [JsonProperty("from_node_table_id")]
         public int From { get; set; }
 
+        [JsonProperty("to_node_table_id")]
         public int To { get; set; }
+
+        [JsonProperty("attributes")]
+        public IList<Attribute> Attributes { get; set; }
     }
 }
--- CSharp/SqlServer2019Graph/Model/Node.cs
+++ CSharp/SqlServer2019Graph/Model/Node.cs
@@ -1,18 +1,20 @@
 ﻿// Copyright (c) Philipp Wagner. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace SqlServer2019Graph.Model
 {
     public class Node
     {
-        [JsonProperty("id")]
+        [JsonProperty("node_name")]
         public string Id { get; set; }
 
         [JsonProperty("name")]
         public string Name { get; set; }
 
-        public TYPE Type { get; set; }
+        [JsonProperty("attributes")]
+        public IList<Attribute> Attributes { get; set; }
     }
 }
